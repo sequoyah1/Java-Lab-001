a18f05c27a99772da2e422a061c18b0e07eaf09e
sequoyah1 <sequoyahschaefer@yahoo.com>
2023-01-21 02:23:02 -0500

    Completed Java lab 001

--- src/HelloXwing.java
+++ src/HelloXwing.java
@@ -1,3 +1,11 @@
+/**
+ *
+ * @author Trevor Hartman
+ * @author Sequoyah Schaefer
+ *
+ * @since Version 1.0
+ *
+ */
 public class HelloXwing {
     /**
      * Returns a String containing a X-Wing ASCII-Art
@@ -28,5 +36,6 @@ public class HelloXwing {
     }
 
     public static void main(String[] args) {
+        System.out.println(HelloXwing.xwing());
     }
 }
